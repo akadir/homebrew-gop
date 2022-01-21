ca44583fd694e9ccd825d9ab3d082a60bcd01e29
goreleaserbot <goreleaser@carlosbecker.com>
2022-01-22 00:40:34 +0300

    Brew formula update for gop version v0.6.0

--- Formula/gop.rb
+++ Formula/gop.rb
@@ -5,20 +5,20 @@
 class Gop < Formula
   desc "simple cli app to open current git repository's remote url in the default browser."
   homepage "https://github.com/akadir/gop"
-  version "0.5.0"
+  version "0.6.0"
 
   on_macos do
-    if Hardware::CPU.intel?
-      url "https://github.com/akadir/gop/releases/download/v0.5.0/gop_0.5.0_macOS_64bit.tar.gz"
-      sha256 "c4bca737e88d6e096f6c67d23aa4e4b7c02a849b46a37cd529911b7205317996"
+    if Hardware::CPU.arm?
+      url "https://github.com/akadir/gop/releases/download/v0.6.0/gop_0.6.0_macOS_ARM64.tar.gz"
+      sha256 "3da75215fd094a13bebd339ab00c5c6209905eb3b5ff5fcd79a63fce9f465f65"
 
       def install
         bin.install "gop"
       end
     end
-    if Hardware::CPU.arm?
-      url "https://github.com/akadir/gop/releases/download/v0.5.0/gop_0.5.0_macOS_ARM64.tar.gz"
-      sha256 "2d5ce34748cce1736e64480ea309a6fc69b60e111042798d1b85fdf71e5ba578"
+    if Hardware::CPU.intel?
+      url "https://github.com/akadir/gop/releases/download/v0.6.0/gop_0.6.0_macOS_64bit.tar.gz"
+      sha256 "074fe96ce444abf8d75adaebe83bfa3ddabd4c5e714f635ca9c02b3248d35686"
 
       def install
         bin.install "gop"
@@ -28,16 +28,16 @@ class Gop < Formula
 
   on_linux do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/akadir/gop/releases/download/v0.5.0/gop_0.5.0_Linux_ARM64.tar.gz"
-      sha256 "e9e02587352f5826cca97f9ba49795bd554bcc83dc60cd8972793470ff064fc5"
+      url "https://github.com/akadir/gop/releases/download/v0.6.0/gop_0.6.0_Linux_ARM64.tar.gz"
+      sha256 "b5385271bdd940ad49542f8a278677be0b63b7518d5ae1d6f63df86c3f70158c"
 
       def install
         bin.install "gop"
       end
     end
     if Hardware::CPU.intel?
-      url "https://github.com/akadir/gop/releases/download/v0.5.0/gop_0.5.0_Linux_64bit.tar.gz"
-      sha256 "6dfb3a01dee8808fe2926467ed8cd6c143c49b4d8a0a7d826418885d20eb82b2"
+      url "https://github.com/akadir/gop/releases/download/v0.6.0/gop_0.6.0_Linux_64bit.tar.gz"
+      sha256 "e20b15223261707c76b86dab94ce749b83ca0d94398a26a0c134fe39e0e8679c"
 
       def install
         bin.install "gop"
